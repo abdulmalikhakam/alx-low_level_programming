2b5f2381cbf552f89a9344aac2b3bf6c2cd7ad81
abdulmalik <abdulmalikhakam14@gmail.com>
2023-06-17 12:33:01 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * discription:'print alphabetical lowercase'
+ * Return: always 0
+ */
+int main(void)
+{
+	putchar(65);
+	putchar('\n');
+	return (0);
+}
