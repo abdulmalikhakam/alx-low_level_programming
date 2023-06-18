bfeaa5e3a86f9f9bdf65522f11f33c79333ca89f
abdulmalik <abdulmalikhakam14@gmail.com>
2023-06-18 15:18:15 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,20 +5,21 @@
  * Return: always 0
 */
 int main(void)
-{int p, l;
+{
+	int n, k;
 
-	for (l = 0; l < 100; l++)
+	for (n = 0; n < 100; n++)
 	{
-		for (p = 0; p < 100; p++)
+		for (k = 0; k < 100; k++)
 		{
-			if (p < l)
+			if (n < k)
 			{
-				putchar((p / 10) + 48);
-				putchar((p % 10) + 48);
+				putchar((n / 10) + 48);
+				putchar((n % 10) + 48);
 				putchar(' ');
-				putchar((l / 10) + 48);
-				putchar((p % 10) + 48);
-				if (p != 98 || l != 99)
+				putchar((k / 10) + 48);
+				putchar((k % 10) + 48);
+				if (n != 98 || k != 99)
 				{
 					putchar(',');
 					putchar(' ');
@@ -30,5 +31,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
-
