a80c5f70e2cc7c327a29d3c10b1ed735d3af8d9c
abdulmalik <abdulmalikhakam14@gmail.com>
2023-06-18 06:24:59 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * discription:'print lower alphabets q and e not included'
+ * Return: always 0
+*/
+int main(void)
+{
+	char lower = 'a';
+
+	while (lower <= 'z')
+	{
+	if (lower != 'q' && lower != 'e')
+		putchar(lower);
+		lower++;
+	}
+	putchar('\n');
+	return (0);
+}
