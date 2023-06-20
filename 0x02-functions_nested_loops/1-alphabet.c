eaae4804844e816fd78676995c1bf07db208e939
abdulmalik <abdulmalikhakam14@gmail.com>
2023-06-20 16:14:10 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * discription: 'lowercase alpahbets'
+ * Return:always 0
+*/
+void print_alphabet(void);
+{
+	char lu;
+
+
+	for (lu = 'a'; lu <= 'z'; lu++)
+		_putchar(lu);
+
+
+	_putchar('\n');
+}
