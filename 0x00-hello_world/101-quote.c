1a364af7a434b88ae19ba979abec0a68d456ea3e
abdulmalik <abdulmalikhakam14@gmail.com>
2023-06-23 13:51:06 -0700

    4-print_most_numbers.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * description:'print standard error'
+ * Return: always 0
+*/
+int main(void)
+{
+	fwrite (2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
