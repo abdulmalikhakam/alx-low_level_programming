31d0cc691d177d66990644a5bb5aab6ecb349ab3
abdulmalik <abdulmalikhakam14@gmail.com>
2023-06-27 07:39:56 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - print half of the string
+ * @str: the string to print
+ * return: void
+*/
+void puts_half(char *str)
+{
+	int j = 0;
+	int k;
+
+	while (str[j] != '\0')
+	{
+	j++;
+	}
+
+	if (j % 2 == 1)
+	{
+	k  = (j - 1) / 2;
+	k += 1;
+	}
+}
